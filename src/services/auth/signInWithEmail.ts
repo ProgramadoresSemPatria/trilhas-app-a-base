e7ef4ddc0d22f2593fe4661d0dbebaddf929efc3
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-05-06 08:09:00 -0300

    feat: create student sign-in flow

--- src/services/auth/signInWithEmail.ts
+++ src/services/auth/signInWithEmail.ts
@@ -1,33 +1,47 @@
-import { child, get, getDatabase, ref } from "firebase/database";
+import { browserLocalPersistence, setPersistence, signInAnonymously } from "firebase/auth";
+import { child, get, getDatabase, ref, set } from "firebase/database";
+import { auth } from "../../lib/firebase";
+import ErrorToast from "../../components/toasts/error-toast";
+import SuccessToast from "../../components/toasts/success-toast";
 
 interface User {
     userId: string;
     email: string;
     learningPath: string;
     role: string;
+    name: string;
 }
 
 export default async function SignInWithEmail(email: string) {
     let resultUser = null,
-        error = null;
+        error = null
     try {
+
        const dbRef = ref(getDatabase());
-       const snapshot = await get(child(dbRef, 'users'));
+       const snapshot = await get(child(dbRef, 'students'));
+       if(snapshot.exists()) {
+           const users = snapshot.val();
+           const userInfo = Object.values(users).find((user) => (user as User).email === email);
+           
+           if(userInfo) {
+                await setPersistence(auth, browserLocalPersistence);
+                const result = await signInAnonymously(auth);
+                const uid = result?.user.uid;
+                localStorage.setItem("session-id", uid ?? "");
+                set(ref(getDatabase(), "students/" + (userInfo as User).userId + "/sessionId"), uid);
 
-        if(snapshot.exists()) {
-            const users = snapshot.val();
-            const userInfo = Object.values(users).find((user) => (user as User).email === email);
-
-            if(userInfo) {
                 resultUser = userInfo;
+                SuccessToast({ message: "Usuário logado com sucesso." });
             }
             else {
                 resultUser = null;
+                ErrorToast({ message: "Usuário não encontrado." });
                 throw new Error("User not found");
             }
         }
         else {
             resultUser = null;
+            ErrorToast({ message: "Usuário não encontrado." });
             throw new Error("User not found");
         }
 
