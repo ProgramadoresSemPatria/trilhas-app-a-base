2e804ed95a4e6761ce686c547d6f41e94b24cd4f
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-05-06 08:19:58 -0300

    feat: add routes for TrilhasDesenvolvedor, TrilhasDados and Aulas pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Painel from "./pages/painel"
 import AuthGuard from "./components/auth-guard"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Toaster } from 'sonner';
+import AdminGuard from "./components/admin-guard"
+import TrilhasDesenvolvedor from "./pages/trilhas-desenvolvedor"
+import TrilhasDados from "./pages/trilhas-dados"
+import Aulas from "./pages/aulas"
 
 function App() {
 
@@ -16,7 +20,33 @@ function App() {
         <Routes>
             <Route path="/" element={<SignIn />} />
             <Route path="/sign-in" element={<SignIn />} />
-            <Route path="/painel" element={<AuthGuard><Painel /></AuthGuard>} />
+            <Route path="/painel" element={
+              <AuthGuard>
+                <AdminGuard>
+                  <Painel />
+                </AdminGuard>
+              </AuthGuard>
+              } />
+            <Route path="/trilhas-desenvolvedor" element={
+              <AuthGuard>
+                  <TrilhasDesenvolvedor />
+              </AuthGuard>
+              } />
+            <Route path="/trilhas-dados" element={
+              <AuthGuard>
+                  <TrilhasDados />
+              </AuthGuard>
+              } />
+              <Route path="/trilhas-desenvolvedor/:slug" element={
+                <AuthGuard>
+                    <Aulas />
+                </AuthGuard>
+                } />
+            <Route path="/trilhas-dados/:slug" element={
+              <AuthGuard>
+                  <Aulas />
+              </AuthGuard>
+              } />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
