761cf2804065ad7c2b798dd81514f55105a8db0c
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-05-07 09:14:17 -0300

    feat: add verification if email is alreay registered

--- src/services/saveStudent.ts
+++ src/services/saveStudent.ts
@@ -1,10 +1,26 @@
-import { getDatabase, push, ref, set } from "firebase/database";
+import { get, getDatabase, push, ref, set } from "firebase/database";
 import { StudentType } from "../utils/types";
+import ErrorToast from "../components/toasts/error-toast";
 
 export default async function saveStudent(student: StudentType) {
 
     const dbRef = ref(getDatabase(), "students/");
     const studentKey = push(dbRef).key;
+
+    const snapshot = await get(dbRef);
+
+    if (snapshot.exists()) {
+        const students = snapshot.val();
+        console.log('students', students)
+        const emailExists = Object.values(students).some(
+            (studentFromDB) => (studentFromDB as StudentType).email === student.email
+        );
+        
+      if (emailExists) {
+        ErrorToast({ message: "Já existe um aluno com esse email." });
+        return 409;
+      }
+    }
 
     student.userId = studentKey;
     const studentRouteRef = ref(getDatabase(), "students/" + studentKey);
@@ -13,7 +29,7 @@ export default async function saveStudent(student: StudentType) {
         await set(studentRouteRef, student);
         return 201;
     } catch (e) {
-        console.log(e);
+        console.error('Error saving student:', e);
         return 500;
     }
 }
